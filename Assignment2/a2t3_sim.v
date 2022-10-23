a68d2e99e664b3169a191db6124dbfd2a4ccbace
SnowCharm <619022098@qq.com>
2022-10-23 16:18:41 +0800

    add a2t3 df and bd

---
+++ Assignment2/a2t3_sim.v
@@ -0,0 +1,17 @@
+// a2t3_sim.v
+
+module a2t3_sim();
+
+reg [3:0] sx;
+wire [3:0] sy_df, sy_bd;
+
+a2t3_df df(.x(sx), .y(sy_df));
+a2t3_bd bd(.x(sx), .y(sy_bd));
+
+initial begin
+    sx = 4'b0;
+    repeat(15) #10 sx = sx + 1;
+    #10 $finish();
+end
+
+endmodule
